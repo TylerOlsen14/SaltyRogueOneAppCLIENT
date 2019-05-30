7adcdf3d129d09840a3a45ef455d338c08e8a443
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-05-30 16:31:13 -0600

    App all set up, need to fill out

--- salty-rogue-one-app/src/App.js
+++ salty-rogue-one-app/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css';
+// import Navigation from './components/Navigation'
+import Home from './components/Home'
+// import Address from './components/address/Address'
+// import Records from './components/records/Records'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <Navigation /> */}
+        <Switch>
+          <Route path='/' exact component={Home} />
+          {/* <Route path='/Address' exact component={Address} />
+          <Route path='/Records' exact component={Records} /> */}
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 

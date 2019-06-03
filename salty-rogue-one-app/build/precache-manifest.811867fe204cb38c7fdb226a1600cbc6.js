8ae60ee69eb66250a4a7609e8c08694411554715
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-06-03 12:55:45 -0600

    ran NPM run build

---
+++ salty-rogue-one-app/build/precache-manifest.811867fe204cb38c7fdb226a1600cbc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c924ace773d920f6667903b72d4bfe29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441e10f2f31d360730cb",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3774e7c6e7c21d68e602",
+    "url": "/static/css/main.14f0249d.chunk.css"
+  },
+  {
+    "revision": "441e10f2f31d360730cb",
+    "url": "/static/js/2.bbeec73e.chunk.js"
+  },
+  {
+    "revision": "3774e7c6e7c21d68e602",
+    "url": "/static/js/main.47438b27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76c57045c5e0e11eef19512f25a2bb12",
+    "url": "/static/media/SaltyRogueAddressScrapeScreeShot.76c57045.png"
+  },
+  {
+    "revision": "c4091322aa4b3ee838ef473fa6900827",
+    "url": "/static/media/SaltyRoguePhoneRecordsScreenShot.c4091322.png"
+  },
+  {
+    "revision": "52b14cf99ed0b81a02363535862e749f",
+    "url": "/static/media/logo.52b14cf9.jpg"
+  }
+]);
